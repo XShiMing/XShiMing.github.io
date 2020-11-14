6ffcb37becefc12eb932749caf99f76441186081
shiming <xshiming@foxmail.com>
2020-11-14 11:53:09 +0800

    Site updated: 2020-11-14 11:52:55

---
+++ js/delban.js
@@ -0,0 +1,21 @@
+// var full_page = document.getElementsByClassName("full_page");
+// if (full_page.length != 0) {
+//   full_page[0].style.background = "transparent";
+// }
+
+// var index = Math.floor(Math.random() * 110)
+// var imageUrl = "https://xqm-oss.oss-cn-beijing.aliyuncs.com/guidao/pic-" + index + ".jpg"
+// var postBg = document.getElementsByClassName("post-bg");
+// if (postBg.length != 0) {
+//   postBg[0].style.background = "url(" + imageUrl + ")";
+// }
+
+var postBg = document.getElementsByClassName("post-bg");
+if (postBg.length != 0 && postBg[0].style.backgroundImage == '') {
+  // postBgBefore[0]
+  //   postBg[0].style.background = "url(" + imageUrl + ")";
+  // postBg[0].sty('post-bg noimg')
+  $('.post-bg').removeClass('post-bg').addClass('post-bgnoimg');
+  // console.log('1');
+  // console.log(document.getElementsByClassName("post-bg"));
+}
---
+++ js/jsdeliver.js
@@ -0,0 +1,12 @@
+var name = "XShiMing";
+var repository = "XShiMing.github.io";
+if (document.domain != 'localhost') {
+  if (document.getElementById("post")) {
+    for (var key in document.getElementById("article-container").getElementsByTagName('img')) {
+      if (isNaN(key)) {
+        break;
+      }
+      document.getElementById("article-container").getElementsByTagName('img')[key].src = document.getElementById("article-container").getElementsByTagName('img')[key].src.replace(document.domain, "cdn.jsdelivr.net/gh/" + name + "/" + repository)
+    }
+  }
+}
